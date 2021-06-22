46264b803a721579df9eb4f7adc6aad97bf90c34
RiericksonT <riericksontorres@gmail.com>
2021-06-22 20:07:57 -0300

    Aula02-AuthGoogle

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -18,12 +18,36 @@ type AuthContextProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextType);
 
-const [user, setUser] = useState<User>();
+export function AuthContextProvider(props: AuthContextProviderProps) {
 
-useEffect(() => {
-  const unsubiscribe = auth.onAuthStateChanged(user => {
-    if (user) {
-      const { displayName, photoURL, uid } = user
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    const unsubiscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        const { displayName, photoURL, uid } = user
+
+        if (!displayName || !photoURL) {
+          throw new Error("Faltam informações da sua conta Google");
+        }
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL,
+          logado: true
+        })
+      }
+    })
+    return () => {
+      unsubiscribe();
+    }
+  }, [])
+
+  async function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const result = await auth.signInWithPopup(provider);
+    if (result.user) {
+      const { displayName, photoURL, uid } = result.user
 
       if (!displayName || !photoURL) {
         throw new Error("Faltam informações da sua conta Google");
@@ -35,32 +59,9 @@ useEffect(() => {
         logado: true
       })
     }
-  })
-  return () => {
-    unsubiscribe();
   }
-}, [])
-
-async function signInWithGoogle() {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  const result = await auth.signInWithPopup(provider);
-  if (result.user) {
-    const { displayName, photoURL, uid } = result.user
-
-    if (!displayName || !photoURL) {
-      throw new Error("Faltam informações da sua conta Google");
-    }
-    setUser({
-      id: uid,
-      name: displayName,
-      avatar: photoURL,
-      logado: true
-    })
-  }
-}
 
 
-export function AuthContextProvider(props: AuthContextProviderProps) {
   return (
     <AuthContext.Provider value={{ user, signInWithGoogle }}>
       {props.children}
